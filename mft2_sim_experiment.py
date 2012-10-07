5bf204ae99e0885e71e1bd6388dc7ccc37518965
flowingdust <mingyuanzhang2000@gmail.com>
2012-10-07 14:53:32 -0500

    update mft2

--- mft2_sim_experiment.py
+++ mft2_sim_experiment.py
@@ -15,12 +15,21 @@ from sklearn.grid_search import IterGrid
 
 TRAINED_MODEL = {}
 
-back_range = range(10, 16, 1)
-front_range = range(10, 16, 1)
-factor_range = [4]
-param_space = {'back': back_range, 'front': front_range}
-#param_list = list(IterGrid(param_space))
-param_list = [{'back':item[0], 'front':item[1]} for item in zip(back_range, front_range)]
+mv_back_range = range(11, 12, 1)
+mv_front_range = range(11, 12, 1)
+mv_factor_range = [4]
+mv_param_space = {'back': mv_back_range, 'front': mv_front_range}
+#mv_param_list = list(IterGrid(mv_param_space))
+mv_param_list = [{'back':item[0], 'front':item[1]} for item in zip(mv_back_range, mv_front_range)]
+
+
+tf_back_range = range(1, 2, 1)
+tf_front_range = range(1, 2, 1)
+tf_factor_range = [3]
+tf_param_space = {'back': tf_back_range, 'front': tf_front_range}
+#tf_param_list = list(IterGrid(tf_param_space))
+tf_param_list = [{'back':item[0], 'front':item[1]} for item in zip(tf_back_range, tf_front_range)]
+
 
 def open_to_open_ret(rec, name=''):
     ret = np.diff(rec['Open']) / rec['Open'][:-1]
@@ -97,7 +106,7 @@ def pos_trading_score(mat, loading, back, front, mean_revert=True):
     pos_loading[np.where(loading < 0)] = 0.0
     pos_factor_front = np.convolve(np.dot(mat, pos_loading), front_weights)[(back+front-1):nn]
     neg_loading = np.copy(loading) * (-1.0)
-    neg_loading[np.where(loading > 0)] = 0.0
+    neg_loading[np.where(loading < 0)] = 0.0
     neg_factor_front = np.convolve(np.dot(mat, neg_loading), front_weights)[(back+front-1):nn]
     
     if mean_revert:
@@ -152,44 +161,72 @@ def train_func(result, dates):
     pca = PCA()
     pca.fit(new_result_mat)
 
-    est = []
-    for ind in factor_range:
-        for param in param_list:
-            for mv in [True]:
-                value = pos_trading_score(new_result_mat, pca.components_[ind], param['back'], param['front'], mv)
-                est.append((ind, param['back'], param['front'], mv, value))
-    est = np.array(est, dtype=[('factor_ind', int), ('back', int), ('front', int), ('mean_revert', bool), ('value', float)])
+    mv_est = []
+    mv = True
+    for ind in mv_factor_range:
+        for param in mv_param_list:
+            value = pos_trading_score(new_result_mat, pca.components_[ind], param['back'], param['front'], mv)
+            mv_est.append((ind, param['back'], param['front'], mv, value))
+    mv_est = np.array(mv_est, dtype=[('factor_ind', int), ('back', int), ('front', int), ('mean_revert', bool), ('value', float)])
+    tf_est = []
+    mv = False
+    for ind in tf_factor_range:
+        for param in tf_param_list:
+            value = pos_trading_score(new_result_mat, pca.components_[ind], param['back'], param['front'], mv)
+            tf_est.append((ind, param['back'], param['front'], mv, value)) 
+    tf_est = np.array(tf_est, dtype=[('factor_ind', int), ('back', int), ('front', int), ('mean_revert', bool), ('value', float)])   
     model_name = max(dates)
-    ind_max = np.argmax(est['value'])
-    TRAINED_MODEL[model_name] = {'factor_ind': est['factor_ind'][ind_max],
-                                 'back': est['back'][ind_max],
-                                 'front': est['front'][ind_max],
-                                 'mean_revert': est['mean_revert'][ind_max],
-                                 'est': est,
+    mv_ind_max = np.argmax(mv_est['value'])
+    tf_ind_max = np.argmax(tf_est['value'])
+    TRAINED_MODEL[model_name] = {'mv_factor_ind': mv_est['factor_ind'][mv_ind_max],
+                                 'mv_back': mv_est['back'][mv_ind_max],
+                                 'mv_front': mv_est['front'][mv_ind_max],
+                                 'mv_est': mv_est,
+                                 'tf_factor_ind': tf_est['factor_ind'][tf_ind_max],
+                                 'tf_back': tf_est['back'][tf_ind_max],
+                                 'tf_front': tf_est['front'][tf_ind_max],
+                                 'tf_est': tf_est,
                                  'loadings': pca.components_,
                                  'dates': dates}
-    param = np.array([(model_name, est['factor_ind'][ind_max], est['back'][ind_max], est['front'][ind_max], est['mean_revert'][ind_max],
-                       est['value'][ind_max])],
-                     dtype = [('model_name', 'S8'), ('factor_ind', int), ('back', int), ('front', int), ('mean_revert', bool),
-                              ('value', float)])
+    param = np.array([(model_name, mv_est['factor_ind'][mv_ind_max], mv_est['back'][mv_ind_max], mv_est['front'][mv_ind_max], mv_est['value'][mv_ind_max],
+                       tf_est['factor_ind'][tf_ind_max], tf_est['back'][tf_ind_max], tf_est['front'][tf_ind_max], tf_est['value'][tf_ind_max])],
+                     dtype = [('model_name', 'S8'), ('mv_factor_ind', int), ('mv_back', int), ('mv_front', int), ('mv_value', float),
+                              ('tf_factor_ind', int), ('tf_back', int), ('tf_front', int), ('tf_value', float)])
     print "optimized param"
     print param
     return param
 
 
 def sim_func(result, dates, param):
-    back = param['back'][0]
-    front = param['front'][0]
+    mv_back = param['mv_back'][0]
+    mv_front = param['mv_front'][0]
+    tf_back = param['tf_back'][0]
+    tf_front = param['tf_front'][0]
     model_name = param['model_name'][0]
     pre_dates = TRAINED_MODEL[model_name]['dates']
-    all_dates = pre_dates[(-back-front):] + dates
-    all_dates_dt = [datetime.datetime.strptime(dt, '%Y%m%d').date() for dt in all_dates]
-    new_result = subset_result_for_dates(result, all_dates_dt)
-    new_result_mat = to_matrix(new_result)
-    trade_dates, position = generating_pos_position(new_result_mat, TRAINED_MODEL[model_name]['loadings'][param['factor_ind'][0]], 
-                                                    all_dates, back, front, mean_revert=param['mean_revert'][0])
-    future_return = new_result_mat[(back+front-1):,:]
-    pnl_mat = np.array(position) * future_return
+    mv_all_dates = pre_dates[(-mv_back-mv_front):] + dates
+    mv_all_dates_dt = [datetime.datetime.strptime(dt, '%Y%m%d').date() for dt in mv_all_dates]
+    mv_new_result = subset_result_for_dates(result, mv_all_dates_dt)
+    mv_new_result_mat = to_matrix(mv_new_result)
+    mv_trade_dates, mv_position = generating_pos_position(mv_new_result_mat, TRAINED_MODEL[model_name]['loadings'][param['mv_factor_ind'][0]], 
+                                                          mv_all_dates, mv_back, mv_front, mean_revert=True)
+    tf_all_dates = pre_dates[(-tf_back-tf_front):] + dates
+    tf_all_dates_dt = [datetime.datetime.strptime(dt, '%Y%m%d').date() for dt in tf_all_dates]
+    tf_new_result = subset_result_for_dates(result, tf_all_dates_dt)
+    tf_new_result_mat = to_matrix(tf_new_result)
+
+    tf_trade_dates, tf_position = generating_pos_position(tf_new_result_mat, TRAINED_MODEL[model_name]['loadings'][param['tf_factor_ind'][0]], 
+                                                          tf_all_dates, tf_back, tf_front, mean_revert=True)
+
+    mv_position = np.array(mv_position)
+    tf_position = np.array(tf_position)
+    #if param['mv_value'] < 0:
+    #mv_position = np.zeros(mv_position.shape)
+    #if param['tf_value'] < 0:
+    #tf_position = np.zeros(tf_position.shape)
+    position = mv_position + tf_position
+    future_return = mv_new_result_mat[(mv_back+mv_front-1):,:]
+    pnl_mat = position * future_return
     pnl = np.sum(pnl_mat, axis=1)
     pos = np.sum(position, axis=1)
     pnl_array = np.array(zip(dates, pnl, pos, pos, pos), dtype=[('date', 'S8'), ('total_pnl', float),
@@ -205,7 +242,7 @@ rsim.SimFunc = sim_func
 
 #fileroot = '/home/mingyuan/Projects/DZH_data/Daily'
 #filelist = [op.join(fileroot, 'daily_000029_20120623215017.csv')]
-filelist = [
+sector_filelist = [
     '/home/mingyuan/Projects/DZH_data/Daily/daily_600016_20120928144733.csv',
     '/home/mingyuan/Projects/DZH_data/Daily/daily_002230_20120928145038.csv',
     '/home/mingyuan/Projects/DZH_data/Daily/daily_601857_20120928145224.csv',
@@ -216,9 +253,18 @@ filelist = [
     '/home/mingyuan/Projects/DZH_data/Daily/daily_000099_20120928150501.csv',
     ]
 
+bank_filelist = [
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_600016_20121001134109.csv',
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_600000_20121001134258.csv',
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_600036_20121001134436.csv',
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_601169_20121001134635.csv',
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_601998_20121001134751.csv',
+    '/home/mingyuan/Projects/DZH_data/Daily/daily_601009_20121001134859.csv',
+    ]
+
+filelist = sector_filelist 
+
 rsim.load_all_data(filelist, open_to_open_ret, min_date='20040101')
-#param = train_func(rsim._result, rsim._dates[:300])
-#sim_func(rsim._result, rsim._dates[300:400], param)
 rsim.split_dates()
 rsim.run()
 rsim.print_pnl_summary()
