7cd37c738b6deb0e4e19581ae459355a5dc892f6
mingyuan <mingyuanzhang2000@gmail.com>
2012-10-11 12:59:10 -0500

    add adding simulation

---
+++ hft_sim_experiment2.py
@@ -0,0 +1,146 @@
+from hft_sim_lib import *
+from hft_signal_lib import *
+from hft_strat_lib import *
+import numpy as np
+from scipy.optimize import nnls
+from collections import defaultdict
+
+cost = 0.0#0.15 - 0.1 * 0.6
+drawdown = -1000.0
+
+ticksize = 0.25
+#edge = 0.5
+edge_buffer_frac = 0.5
+latency = 0
+#time_in_force = 600
+
+def get_signals(data):
+    decays = [0.5, 0.7, 0.8, 0.9]
+    decay_names = [str(ff).replace('0.', '_') for ff in decays]
+    mid = midpoint(data) 
+    book = booksignal(data, 1, 0.9) - mid
+    last = data['lastprice'] - mid
+    #emabook = [ema(book, ff) - mid for ff in decays]
+    vwap = [vwap_ema(data, ff) - mid for ff in decays]
+    #trends = [book / ema(book, ff) - 1.0 for ff in decays]
+    trsigns = [tradesignema(data, ff) for ff in decays]
+    trsvolumes = [tradesignvolume_ema(data, ff) for ff in decays]
+    sv = [signedvolume_ema(data, ff) for ff in decays]
+    
+    signals = {'book': book, 'last': last}
+    #signals.update(dict(zip(['bkema'+dn for dn in decay_names], emabook)))
+    signals.update(dict(zip(['vwap'+dn for dn in decay_names], vwap)))
+    #signals.update(dict(zip(['trends'+dn for dn in decay_names], trends)))
+    signals.update(dict(zip(['trsign'+dn for dn in decay_names], trsigns)))
+    signals.update(dict(zip(['trsvolume'+dn for dn in decay_names], trsvolumes)))
+    signals.update(dict(zip(['signedv'+dn for dn in decay_names], sv)))
+    return signals
+
+def construct_valuation(data, beta):
+    signals = get_signals(data)
+    valuation = midpoint(data)
+    for key in signals.keys():
+        valuation += signals[key] * beta[key]
+    return valuation
+
+
+def run_regression(result, dates):
+    mid = []
+    future = []
+    sample_freq = 120
+    future_horizon = 120 * 0.5
+    signals = defaultdict(list)
+    for adate in dates:
+        data = result[adate]
+        signals_tmp = get_signals(data)
+        mid_tmp = midpoint(data)
+        base_ind, offset_inds = generate_offset_index(len(data), [future_horizon],
+                                                      base = subsample_nsample(data, sample_freq),
+                                                      burn_in = sample_freq)
+        for key in signals_tmp.keys():
+            signals[key] += list(signals_tmp[key][base_ind])
+        mid +=(list(mid_tmp[base_ind]))
+        future +=(list(mid_tmp[offset_inds[0]]))
+    names = signals.keys()
+    X = np.c_[[np.array(signals[nm]) for nm in names]].T
+    Y = np.array(future) - np.array(mid)
+    #beta, res, rank, s = np.linalg.lstsq(X, Y)
+    beta, rrs = nnls(X, Y)
+    valuation = np.dot(X, beta)
+    print 'Regression Dates  : %s - %s'%(min(dates), max(dates))
+    print 'Regression Results: %s'%(str(dict(zip(names, beta))))
+    return names, dict(zip(names, list(beta))), np.abs(valuation).mean()
+
+def optimize_thresh(result, dates, reg_param):
+    names, beta, val_std = reg_param
+    thresh = np.arange(0.5, 3.5, 0.1) * val_std
+    summary = [None] * len(thresh)
+    for adate in dates:
+        data = result[adate]
+        valuation = construct_valuation(data, beta)
+        for ii, th in enumerate(thresh):
+            if summary[ii] is None:
+                summary[ii] = sim_market_making_simple(data, valuation, th, th * edge_buffer_frac,
+                                                       latency, ticksize)
+            else :
+                summary[ii] = np.append(summary[ii], sim_market_making_simple(data, valuation, th, th * edge_buffer_frac,
+                                                                              latency, ticksize))
+    for ii in range(len(summary)):
+        summary[ii]['total_pnl'] -= (summary[ii]['volume'] * cost)
+    avg_pnl = [np.mean(item['total_pnl'] ) for item in summary]
+    sharpe  = [np.mean(item['total_pnl'] ) / np.std(item['total_pnl'] ) for item in summary]
+    ppv     = [np.sum(item['total_pnl'] ) / np.sum(item['volume']) for item in summary]
+    volume  = [np.mean(item['volume']) for item in summary]
+    sharpe_constrained = [sp if (vol > 950) and (vol < 4450) else -1e10 for (sp, vol) in zip(sharpe, volume)]
+    #print avg_pnl
+    #print sharpe
+    #print ppv
+    idx_max = np.argmax(sharpe_constrained)
+    print 'Optimization Dates  :  %s - %s'%(min(dates), max(dates))
+    print 'Optimization Results:  idx = %d; thresh = %s'%(idx_max, str(thresh[idx_max]))
+    print_summary(summary[idx_max], 'InSample Optimized')
+    if sharpe[idx_max] < 0: ## negative stuff, no trading
+        return 1e10
+    else :
+        return thresh[idx_max]
+
+def optimize_in_sample(result, dates):
+    reg_dates = dates#[:len(dates) / 2]
+    sim_dates = dates#[(len(dates) / 2):]
+    reg_param = run_regression(result, reg_dates)
+    trade_param = optimize_thresh(result, sim_dates, reg_param)
+    param = np.array([tuple([trade_param] + [reg_param[1][nm] for nm in reg_param[0]])],
+                     dtype = [('thresh', 'f')] + [(nm, 'f') for nm in reg_param[0]])
+    return param
+
+def sim_out_sample(result, dates, param):
+    thresh = param['thresh']
+    beta = param[0] #list(param[0])[1:]
+    pnl_array = None
+    for adate in dates:
+        data = result[adate]
+        valuation = construct_valuation(data, beta)
+        if pnl_array is None:
+            pnl_array = sim_market_making_simple(data, valuation, thresh, thresh * edge_buffer_frac,
+                                                 latency, ticksize)
+        else :
+            pnl_array = np.append(pnl_array, sim_market_making_simple(data, valuation, thresh, thresh * edge_buffer_frac,
+                                                                      latency, ticksize))
+    pnl_array['total_pnl'] -= (pnl_array['volume'] * cost)
+    print 'OutSample Dates : %s - %s'%(min(dates), max(dates))
+    print_summary(pnl_array, 'Outsampl Result')
+    return pnl_array
+
+
+rsim = RollSim('trigger', 10, 5)
+rsim.TrainFunc = optimize_in_sample
+rsim.SimFunc = sim_out_sample
+
+rsim.YearMonthList = ['201101']
+
+rsim.load_all_data()
+rsim.split_dates()
+rsim.run()
+rsim.print_pnl_summary()
+
+
--- hft_strat_lib.py
+++ hft_strat_lib.py
@@ -143,3 +143,243 @@ def sim_trigger_drawdown(data, valuation, thresh, drawdown):
     price_diff = np.diff(mid)
     return get_summary_simple_drawdown(position, price_diff, drawdown)
 
+def is_filled(limit_price, side, data_idx, data, latency, time_in_force):
+    in_action_begin = data_idx + latency
+    in_action_end = min(data_idx + latency + time_in_force, len(data))
+    trade_price = data['lastprice']
+    trade_price_in_action = trade_price[in_action_begin:in_action_end]
+    if side > 0:
+        filled = np.any(trade_price_in_action < limit_price)
+        if filled:
+            fill_ts = np.min(np.where(trade_price_in_action < limit_price)) + in_action_begin
+        else :
+            fill_ts = in_action_end
+        return filled, fill_ts
+    else :
+        filled = np.any(trade_price_in_action > limit_price)
+        if filled:
+            fill_ts = np.min(np.where(trade_price_in_action > limit_price)) + in_action_begin
+        else :
+            fill_ts = in_action_end
+        return filled, fill_ts
+
+def get_limit_price(valuation, edge, ticksize):
+    bid_price = np.floor((valuation - edge) / ticksize) * ticksize
+    ask_price = np.floor((valuation + edge) / ticksize + 0.5) * ticksize
+    return bid_price, ask_price
+
+def get_summary_simple_drawdown_limitorder(target_position, data, drawdown,
+                                           bid_price, ask_price,
+                                           latency, time_in_force, iteration=1000):
+    signal = np.array([0] + np.diff(target_position).tolist())
+    trade_idx = np.where(np.abs(signal) > 0)[0]
+    order_qty = np.zeros(len(signal))
+    filled_qty = np.zeros(len(signal))
+    filled_price = np.zeros(len(signal))
+    prefilled_qty = np.zeros(len(signal))
+    prefilled_price = np.zeros(len(signal))
+    previous_idx = 0
+    buy_order_expiring = []
+    sell_order_expiring = []
+    #import pdb; pdb.set_trace()
+    for idx in trade_idx:
+        cur_position = np.sum(filled_qty[:idx])
+        side = ((signal[idx] * cur_position) <= 0) * signal[idx]
+        buy_order_expiring = [ts for ts in buy_order_expiring if ts > idx]
+        sell_order_expiring = [ts for ts in sell_order_expiring if ts > idx]
+        if len(buy_order_expiring) >= iteration and side > 0:
+            continue
+        if len(sell_order_expiring) >= iteration and side < 0:
+            continue
+        if side == 0:
+            continue
+        elif side > 0:
+            limit_price = bid_price[idx]
+            order_qty[idx] = signal[idx] 
+            filled, fill_ts = is_filled(limit_price, side, idx+1, data, latency, time_in_force)
+            if filled:
+                prefilled_qty[idx] = order_qty[idx]
+                prefilled_price[idx] = limit_price
+                filled_qty[fill_ts] += order_qty[idx]
+                filled_price[fill_ts] = limit_price
+            buy_order_expiring.append( fill_ts )
+        else:
+            limit_price = ask_price[idx]
+            order_qty[idx] = signal[idx] 
+            filled, fill_ts = is_filled(limit_price, side, idx+1, data, latency, time_in_force)
+            if filled:
+                prefilled_qty[idx] = order_qty[idx]
+                prefilled_price[idx] = limit_price
+                filled_qty[fill_ts] += order_qty[idx]
+                filled_price[fill_ts] = limit_price
+            sell_order_expiring.append( fill_ts )
+
+    cum_position = np.cumsum(prefilled_qty)
+    result = get_blotter_pnl(order_qty, prefilled_qty, prefilled_price, cum_position, data,
+                             drawdown)
+    return result
+
+def get_summary_simple_drawdown_limitorder_deprecated(target_position, data, drawdown,
+                                           bid_price, ask_price,
+                                           latency, time_in_force):
+    signal = np.array([0] + np.diff(target_position).tolist())
+    trade_idx = np.where(np.abs(signal) > 0)[0]
+    order_qty = np.zeros(len(signal))
+    filled_qty = np.zeros(len(signal))
+    filled_price = np.zeros(len(signal))
+    previous_idx = 0
+    buy_order_outstanding = False
+    buy_order_expiring = 0
+    sell_order_outstanding = False
+    sell_order_expiring = 0
+    for idx in trade_idx:
+        cur_position = np.sum(filled_qty[:idx])
+        side = ((signal[idx] * cur_position) <= 0) * signal[idx]
+        if idx >= buy_order_expiring:
+            buy_order_outstanding = False
+        if idx >= sell_order_expiring:
+            sell_order_outstanding = False
+        if buy_order_outstanding and side > 0:
+            continue
+        if sell_order_outstanding and side < 0:
+            continue
+        if side == 0:
+            continue
+        elif side > 0:
+            limit_price = bid_price[idx]
+            order_qty[idx] = signal[idx] 
+            filled, fill_ts = is_filled(limit_price, side, idx, data, latency, time_in_force)
+            if filled:
+                filled_qty[fill_ts] += order_qty[idx]
+                filled_price[fill_ts] = limit_price
+                buy_order_outstanding = True
+                buy_order_expiring = fill_ts
+        else:
+            limit_price = ask_price[idx]
+            order_qty[idx] = signal[idx] 
+            filled, fill_ts = is_filled(limit_price, side, idx, data, latency, time_in_force)
+            if filled:
+                filled_qty[fill_ts] += order_qty[idx]
+                filled_price[fill_ts] = limit_price
+                sell_order_outstanding = True
+                sell_order_expiring = fill_ts
+
+
+    cum_position = np.cumsum(filled_qty)
+    result = get_blotter_pnl(order_qty, filled_qty, filled_price, cum_position, data,
+                             drawdown)
+    return result
+
+
+def get_blotter_pnl(order_qty, filled_qty, filled_price, cum_position, data, drawdown):
+    #import pdb; pdb.set_trace()
+    mid = midpoint(data)
+    cash = np.sum(filled_qty * filled_price) * (-1.0)
+    open_cash = cum_position[-1] * mid[-1]
+    pnl = cash + open_cash
+    pnl_t = np.cumsum(cum_position[:-1] * np.diff(mid))
+    spread = np.cumsum((mid - filled_price) * filled_qty)
+    pnl_t = spread[1:] + pnl_t
+    assert abs(pnl - pnl_t[-1]) < 0.01
+
+    running_max = np.maximum.accumulate(pnl_t)
+    idx = np.where(pnl_t - running_max < drawdown)[0]
+    if len(idx) > 0:
+        stop_idx = np.min(idx)
+        cum_position[(stop_idx+1):] = 0.0
+        pnl_t = np.cumsum(cum_position[:-1] * np.diff(mid))
+        order_qty[(stop_idx+1):] = 0.0
+        filled_qty[(stop_idx+1):] = 0.0
+        spread = np.cumsum((mid - filled_price) * filled_qty)
+        pnl_t = spread[1:] + pnl_t
+
+    order_volume = np.sum(np.abs(order_qty))
+    trade_volume = np.sum(np.abs(filled_qty))
+
+    
+    result = np.array([(pnl_t[-1], np.min(pnl_t), np.max(pnl_t),
+                        np.min(cum_position), np.max(cum_position), trade_volume,
+                        order_volume, trade_volume * 1.0 / order_volume)],
+                      dtype = [('total_pnl', 'f'), ('min_pnl', 'f'),
+                               ('max_pnl', 'f'), ('min_position', int),
+                               ('max_position', int), ('volume', int),
+                               ('order_volume', int), ('fill_ratio', float)])
+    return result
+        
+
+
+def is_filled_simple(bid_limit, ask_limit, trade_price):
+    if trade_price < bid_limit:
+        return 1
+    elif trade_price > ask_limit:
+        return -1
+    else :
+        return 0
+
+def sim_market_making_simple(data, valuation, edge, edge_buffer, latency, ticksize):
+    mid = midpoint(data)
+    bid_limitprice, ask_limitprice = get_limit_price(valuation, edge, ticksize)
+    back_off_factor = 100
+    beg_idx = np.min(np.where(~np.isnan(valuation)))
+    bid_quote = bid_limitprice[beg_idx]
+    ask_quote = ask_limitprice[beg_idx]
+    filled_qty = np.zeros(len(mid))
+    filled_price = np.zeros(len(mid))
+    bid_edge_adjust = 0.0
+    ask_edge_adjust = 0.0
+    #import pdb;pdb.set_trace()
+    for ii in range(beg_idx+1, len(mid)):
+        filled = is_filled_simple(bid_quote, ask_quote, data['lastprice'][ii])
+        if filled > 0:
+            filled_qty[ii] = filled
+            filled_price[ii] = bid_quote
+            bid_edge_adjust = ticksize * back_off_factor
+            ask_edge_adjust = 0.0
+        elif filled < 0:
+            filled_qty[ii] = filled
+            filled_price[ii] = ask_quote
+            ask_edge_adjust = ticksize * back_off_factor
+            bid_edge_adjust = 0.0
+        if valuation[ii] - bid_quote <= edge - edge_buffer:
+            bid_quote = bid_limitprice[ii]
+        if ask_quote - valuation[ii] <= edge - edge_buffer:
+            ask_quote = ask_limitprice[ii]
+        bid_quote -= bid_edge_adjust
+        ask_quote += ask_edge_adjust
+        
+    order_qty = filled_qty.copy()
+    cum_position = np.cumsum(filled_qty)
+    result = get_blotter_pnl(order_qty, filled_qty, filled_price, cum_position, data, drawdown=-100000.0)
+    import pdb;pdb.set_trace()
+    return result
+
+
+
+def sim_trigger_drawdown_limitorder(data, valuation, thresh, drawdown,
+                                    edge, latency, time_in_force, ticksize):
+    mid = midpoint(data)
+    prediction = valuation - mid
+    bid_limitprice, ask_limitprice = get_limit_price(valuation, edge, ticksize)
+    #bid_limitprice = data['bid1']
+    #ask_limitprice = data['ask1']
+    buy_ind = np.where( prediction >= thresh)
+    sell_ind = np.where( prediction <= -thresh)
+    trade_act = np.zeros(len(data))
+    trade_act[buy_ind] = 1
+    trade_act[sell_ind] = -1
+    position = np.zeros(len(data))
+    position[0] = trade_act[0]
+    for ii in range(1, len(data)) :
+        if trade_act[ii] == 0:
+            position[ii] = position[ii-1]
+        else :
+            position[ii] = trade_act[ii]
+    result = get_summary_simple_drawdown_limitorder(position, 
+                                                    data, drawdown,
+                                                    bid_limitprice, ask_limitprice,
+                                                    latency, time_in_force)
+    return result
+
+
+
+        
